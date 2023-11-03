297b5baa95164c968ada97036be53e674518b9c7
Sarankumar9791 <149292541+Sarankumar9791@users.noreply.github.com>
2023-11-03 13:19:26 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-# Python program to find
-# factorial of given number
-import math
+"""
+Write a function called linear_search_product that takes the list of products and a target product
+name as input. The function should perform a linear search to find the target product in the list and
+return a list of indices of all occurrences of the product if found, or an empty list if the product is not
+found.
+"""
 
 
-def fact(n):
-  return (math.factorial(n))
+def linearSearchProduct(productList, targetProduct):
+  indices = []
+
+  for index, product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
+
+  return indices
 
 
-num = int(input("Enter the number:"))
-f = fact(num)
-print("Factorial of", num, "is", f)
+# Example usage:
+products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
+target = "shoes"
+target2 = 'apple'
+result = linearSearchProduct(products, target)
+print(result)
